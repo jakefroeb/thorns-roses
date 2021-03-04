8e7ebc489ab016e543ba31a47cd81af37ddc9f5c
jakefroeb <jake.herman.froeb@gmail.com>
2021-03-03 21:19:45 -0600

    got it working my data was wrong because i had to fill it in myself

--- src/components/nurseries/Nursery.js
+++ src/components/nurseries/Nursery.js
@@ -1,15 +1,14 @@
 export const Nursery = ({nursery, flowers, distributors}) => (
     <section className="nursery">
-    {console.log(nursery,flowers,distributors)}
     <h3 className="nursery__name">{nursery.name}</h3>
     <ul>
         {flowers.map(f => (
-            <li className="nursery__flower">Species: {f.flower.species} Color:{f.flower.color} Price{f.price}</li>
+            <li key={f.id} className="nursery__flower">Species: {f.flower.species}, Color: {f.flower.color}, Price: {f.price}</li>
         ))}
     </ul>
     <ul>
         {distributors.map(d => (
-            <li className="nursery__distributor">{d.distributorId}</li>
+            <li key={d.id} className="nursery__distributor">Distributor: {d.distributor.name}</li>
         ))}
     </ul>
 </section>
